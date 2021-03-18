c0dc637a7dfae109a118b57aee09e4417b1e1664
Exortix <40566204+Exortix@users.noreply.github.com>
2021-03-18 20:36:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+quran = []
+
+function displaySurah() {
+    document.title = quran[Number(document.getElementById('select').value)-1].name;
+    document.getElementById('main').innerHTML = ``;
+    document.getElementById('main').innerHTML += 
+    `<div class="card">
+    <div class="card-body"><h5 class="card-title">${quran[Number(document.getElementById('select').value)-1].name}</h5></div>
+    </div>`;
+    quran[Number(document.getElementById('select').value)-1].ayahs.forEach(e => {
+        document.getElementById('main').innerHTML += 
+        `<div class="card">
+            <div class="card-body"><p class="card-text">${e.text}</p></div>
+        </div>`;
+    });
+}
+
+if (localStorage.getItem('quran') == true) {
+    quran = JSON.parse(localStorage.getItem('quran')).data.surahs;
+    quran.forEach(e => {
+        document.getElementById('select').innerHTML += `<option value = '${e.number}'>${e.name}</option>`;
+    });
+    displaySurah();
+}
+
+
+$.ajax({
+    method:'GET',
+    url:'http://api.alquran.cloud/v1/quran/quran-uthmani',
+    success:function (response) {
+        quran = response.data.surahs;
+        quran.forEach(e => {
+            document.getElementById('select').innerHTML += `<option value = '${e.number}'>${e.name}</option>`;
+        });
+        localStorage.setItem('quran', JSON.stringify(response));
+        displaySurah();
+    },
+    error:function (error) {
+        quran = JSON.parse(localStorage.getItem('quran')).data.surahs;
+        quran.forEach(e => {
+            document.getElementById('select').innerHTML += `<option value = '${e.number}'>${e.name}</option>`;
+        });
+        displaySurah();
+    }
+});
+
+$( "#select" ).change(function() {
+    displaySurah();
+});
