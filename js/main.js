b6b10668ded53b5f006a8045059ff2bc8a60dc22
Exortix <40566204+Exortix@users.noreply.github.com>
2021-03-20 16:45:09 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,35 +16,26 @@ function displaySurah() {
 }
 
 
-if (localStorage.getItem('quran')) {
-    quran = JSON.parse(localStorage.getItem('quran')).data.surahs;
-    quran.forEach(e => {
-        document.getElementById('select').innerHTML += `<option value = '${e.number}'>${e.name}</option>`;
-    });
-    displaySurah();
-}
-else
-{
-    $.ajax({
-        method:'GET',
-        url:'quran-uthmani.json',
-        success:function (response) {
-            quran = response.data.surahs;
-            quran.forEach(e => {
-                document.getElementById('select').innerHTML += `<option value = '${e.number}'>${e.name}</option>`;
-            });
-            localStorage.setItem('quran', JSON.stringify(response));
-            displaySurah();
-        },
-        error:function (error) {
-            quran = JSON.parse(localStorage.getItem('quran')).data.surahs;
-            quran.forEach(e => {
-                document.getElementById('select').innerHTML += `<option value = '${e.number}'>${e.name}</option>`;
-            });
-            displaySurah();
-        }
-    });
-}
+
+$.ajax({
+    method:'GET',
+    url:'quran-uthmani.json',
+    success:function (response) {
+        quran = response.data.surahs;
+        quran.forEach(e => {
+            document.getElementById('select').innerHTML += `<option value = '${e.number}'>${e.name}</option>`;
+        });
+        localStorage.setItem('quran', JSON.stringify(response));
+        displaySurah();
+    },
+    error:function (error) {
+        quran = JSON.parse(localStorage.getItem('quran')).data.surahs;
+        quran.forEach(e => {
+            document.getElementById('select').innerHTML += `<option value = '${e.number}'>${e.name}</option>`;
+        });
+        displaySurah();
+    }
+});
 
 
 
